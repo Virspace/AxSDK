15ef0e48fb9520db9e63887368bc55e93974fe91
Michael DeForge <mddeforge@gmail.com>
2021-11-21 20:45:21 -0500

    Reworked AxArray's and updated tests

--- Foundation/include/Foundation/AxArray.h
+++ Foundation/include/Foundation/AxArray.h
@@ -1,24 +1,173 @@
-#pragma once
+#ifndef AXARRAY_INCLUDE
+#define AXARRAY_INCLUDE
 
 #include "Foundation/Types.h"
 
-typedef struct AxArray AxArray;
+// TODO(mdeforge): Consider removing SetCapacity and SetSize and renaming things to grow/shrink
 
-#define AX_ARRAY(a) AxArray *a = NULL; AxArrayCreate(a);
+/*
+    AxArray's are a type-safe dynamic array. They are useful when building a list of things
+    with an unknown quantity. AxArray's are based on Sean Barrett's stretchy buffer's,
+    [stb_ds.h](https://github.com/nothings/stb/blob/master/stb_ds.h).
 
-void AxArrayCreate(AxArray *Array);
-void AxArrayDestroy(AxArray *Array);
-void AxArrayClear(AxArray *Array);
-bool AxArrayIsEmpty(const AxArray *Array);
-int32_t AxArraySize(const AxArray *Array);
-int32_t AxArraySizeInBytes(const AxArray *Array);
-int32_t AxArrayMaxSize(const AxArray *Array);
-int32_t AxArrayCapacity(const AxArray *Array);
+    AxArray's are accessed using a regular pointer to the array type.
 
-void AxArrayResize(AxArray *Array, int32_t NewSize);
-void AxArrayReserve(AxArray *Array, int32_t NewCapacity);
+    int *array = NULL;
 
-void *AxArrayAt(const AxArray *Array, int32_t Index);
-void AxArrayPushFront(AxArray *Array, const void *Value);
-void AxArrayPushBack(AxArray *Array, const void *Value);
-void *AxArrayInsert(AxArray *Array, const void *It, const void *Value);
+    The Size and Capacity of the array is stored in a header before the array itself.
+
+    [Example]
+    int main(void)
+    {
+        int *array = NULL;
+        ArrayPush(array, 2);
+        ArrayPush(array, 3);
+        ArrayPush(array, 5);
+        for (int i=0; i < ArraySize(array); ++i) {
+            printf("%d ", array[i]);
+        }
+    }
+*/
+
+typedef struct AxArrayHeader
+{
+    size_t Capacity;
+    size_t Size;
+} AxArrayHeader;
+
+#if defined(AxRealloc) && !defined(AxFree) || !defined(AxRealloc) && defined(AxFree)
+#error "You must define both ArrayRealloc and ArrayFree together, or define neither."
+#endif
+#if !defined(AxRealloc) && !defined(AxFree)
+#include <stdlib.h>
+#define AxRealloc(c, p, s) realloc(p, s)
+#define AxFree(c, p)       free(p)
+#endif
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+extern void *ArrayReserve(void* A, size_t BlockSize, size_t NewCapacity);
+
+#ifdef __cplusplus
+}
+#endif
+
+// Returns the header data.
+#define ArrayHeader(a)         ((AxArrayHeader *)((uint8_t *)(a) - sizeof(AxArrayHeader)))
+
+// Frees the array
+#define ArrayFree(a)           ((void) ((a) ? AxFree(NULL, ArrayHeader(a)) : (void)0), (a) = NULL)
+
+// [Capacity]
+
+// Checks whether the container is empty.
+#define ArrayEmpty(a)          (ArraySize(a) > 0 ? false : true)
+
+// Returns the number of elements that can be held in the currently allocated storage.
+#define ArrayCapacity(a)       ((a) ? ArrayHeader(a)->Capacity : 0)
+
+// Sets the number of elements that can be held. Reallocates if larger than current capacity.
+#define ArraySetCapacity(a, c) (ArrayResize(a, c))
+
+// Returns the number of elements
+#define ArraySize(a)           ((a) ? ArrayHeader(a)->Size : 0)
+
+// Sets the number of elements
+#define ArraySetSize(a, n)     ((ArrayCapacity(a) < (size_t)(n) ? ArraySetCapacity((a), (size_t)(n)), 0 : 0), (a) ? ArrayHeader(a)->Size = (size_t)(n) : 0)
+
+// Returns the size of the array in bytes (not including the header data).
+#define ArraySizeInBytes(a)    (ArraySize(a) * sizeof(*(a)))
+
+#define ArrayNeedsGrowth(a, n) ((n) > ArrayCapacity(a))
+
+// [Element Access]
+
+// Access the first element.
+#define ArrayBegin(a)          ((a)[0])
+
+// Returns a pointer one element past the end of the array, or NULL if the array is not allocated.
+#define ArrayEnd(a)            ((a) ? (a) + ArraySize(a) : 0)
+
+// Returns a pointer to the last element, or NULL if the array is empty.
+#define ArrayBack(a)           ((a) ? ArrayEnd(a) - 1 : 0)
+
+// Pops the last element from the array and returns it
+#define ArrayPop(a)            ((a)[--ArrayHeader(a)->Size])
+
+// [Modifiers]
+
+// Adds an element to the end.
+#define ArrayPush(a, v)        (ArrayMaybeResize(a, 1), (a)[ArrayHeader(a)->Size++] = (v))
+
+#define ArrayPushArray(a, b) (ArrayMaybeResize(a, ArraySize(a) + ArraySize(b)), memcpy((a) + ArraySize(a), (b), ArraySize(b) * sizeof(*(a))), ArrayHeader(a)->Size += ArraySize(b))
+
+#define ArrayErase(a, i)       (ArrayEraseN(a, i, 1))
+#define ArrayEraseN(a, i, n)   (memmove(&(a)[i], &(a)[(i)+(n)], sizeof(*(a)) * (ArrayHeader(a)->Size - (n) - (i))), ArrayHeader(a)->Size -= (n))
+#define ArrayEraseSwap(a, i)   ((a)[i] = ArrayBack(a), ArrayHeader(a)->Size -= 1)
+#define ArrayMaybeResize(a, n) ((!(a) || ArrayNeedsGrowth(a, n)) ? ArrayResize(a, ArrayCapacity(a) + (n)) : 0)
+
+// Changes the number of elements stored.
+#define ArrayResize(a, b)      ((a) = ArrayReserveWrapper((a), sizeof *(a), (b)))
+
+#define ArrayGrowAt(a, n)      (())
+
+// Shrinks the number of elements to the specificed value. Will not reallocate.
+#define ArrayShrink(a, n)      ((a) ? ArrayHeader(a)->Size = (n) : 0)
+
+#ifdef __cplusplus
+// Reserves storage.
+template<class T> static T* ArrayReserveWrapper(T *A, size_t BlockSize, size_t NewCapacity) {
+    return (T *)ArrayReserve((void *)A, BlockSize, NewCapacity);
+}
+#else
+// Reserves storage.
+#define ArrayReserveWrapper ArrayReserve
+#endif
+
+#endif // INCLUDE_STB_DS_H
+
+// [Implementation] ///////////////////////////////////////////
+
+#ifdef AXARRAY_IMPLEMENTATION
+// static inline void *ArraySetCapacity(void *A, size_t BlockSize, size_t NewCapacity
+// {
+//     uint8_t *p = A ? (uint8_t *)ArrayHeader(A) : 0;
+//     const size_t BytesBefore = A ? BlockSize * ArrayCapacity(A) + sizeof(AxArrayHeader) : 0;
+//     const size_t BytesAfter = NewCapacity ? BlockSize * NewCapacity + sizeof(AxArrayHeader) : 0;
+
+//     // If   
+//     if (p && (ArrayCapacity(A) == 0)
+//     {
+//         // Perform a static allocation
+
+//     }
+// }
+
+void *ArrayReserve(void *A, size_t BlockSize, size_t NeededCapacity)
+{
+    const size_t Capacity = ArrayCapacity(A);
+    if (Capacity >= NeededCapacity) {
+        return (A);
+    }
+
+    const size_t MinNewCapacity = Capacity ? Capacity * 2 : 16;
+    const size_t NewCapacity = MinNewCapacity > NeededCapacity ? MinNewCapacity : NeededCapacity;
+
+    void* B = AxRealloc(NULL, (A) ? ArrayHeader(A) : 0, (size_t)NewCapacity * BlockSize + sizeof(AxArrayHeader));
+    if (B)
+    {
+        B = (uint8_t*)B + sizeof(AxArrayHeader);
+
+        if (A == NULL) {
+            ArrayHeader(B)->Size = 0;
+        }
+
+        ArrayHeader(B)->Capacity = NewCapacity;
+    }
+
+    return (B);
+}
+
+#endif
--- Foundation/tests/src/AxArrayTests.cpp
+++ Foundation/tests/src/AxArrayTests.cpp
@@ -1,21 +1,177 @@
 #include "gtest/gtest.h"
 #include "Foundation/Types.h"
 
-extern "C"
+//extern "C"
+//{
+#define AXARRAY_IMPLEMENTATION
+#include "Foundation/AxArray.h"
+//}
+
+struct Foo { int32_t a; char *b; };
+#define FooConstruct(n, t) (Foo { n, t })
+
+class AxArrayTest : public testing::Test
 {
-    #include "Foundation/AxArray.h"
+protected:
+    Foo* Arr;
+
+    void SetUp()
+    {
+        Arr = NULL;
+
+        ArrayPush(Arr, FooConstruct(1, "Fly,"));
+        ArrayPush(Arr, FooConstruct(2, "you"));
+        ArrayPush(Arr, FooConstruct(3, "fools!"));
+    }
+
+    void TearDown()
+    {
+        ArrayFree(Arr);
+    }
+};
+
+TEST_F(AxArrayTest, Header)
+{
+    ASSERT_NE(Arr, nullptr);
+
+    EXPECT_EQ(ArrayHeader(Arr)->Capacity, 16);
+    EXPECT_EQ(ArrayHeader(Arr)->Size, 3);
 }
 
-TEST(AxArrayTest, AddElement)
+TEST_F(AxArrayTest, Empty)
 {
-    AX_ARRAY(Foo);
-    AxArrayPushBack(Foo, (void *)10);
-    AxArrayPushBack(Foo, (void *)20);
-    AxArrayPushBack(Foo, (void *)30);
+    EXPECT_FALSE(ArrayEmpty(Arr));
+    ArraySetSize(Arr, 0);
+    EXPECT_TRUE(ArrayEmpty(Arr));
+}
 
-    EXPECT_EQ((int32_t)AxArrayAt(Foo, 0), 10);
-    EXPECT_EQ((int32_t)AxArrayAt(Foo, 1), 20);
-    EXPECT_EQ((int32_t)AxArrayAt(Foo, 2), 30);
-    
-    //EXPECT_STREQ(Result2, "Second address");
+// TODO(mdeforge): Review the stb API again and think about if those simple commands will suffice
+// Also add a insert and remove at, look at the std page for good details on how
+
+TEST_F(AxArrayTest, Capacity)
+{
+    EXPECT_EQ(ArrayCapacity(Arr), 16);
+}
+
+TEST_F(AxArrayTest, SetCapacity)
+{
+    EXPECT_EQ(ArrayCapacity(Arr), 16);
+    ArraySetCapacity(Arr, 32);
+    EXPECT_EQ(ArrayCapacity(Arr), 32);
+}
+
+TEST_F(AxArrayTest, ZeroSize)
+{
+    Foo* ZeroArr = NULL;
+    EXPECT_EQ(ArraySize(ZeroArr), 0);
+}
+
+TEST_F(AxArrayTest, Size)
+{
+    EXPECT_EQ(ArraySize(Arr), 3);
+}
+
+TEST_F(AxArrayTest, SetSize)
+{
+    EXPECT_EQ(ArraySize(Arr), 3);
+    ArraySetSize(Arr, 1);
+    EXPECT_EQ(ArraySize(Arr), 1);
+}
+
+TEST_F(AxArrayTest, SizeInBytes)
+{
+    EXPECT_EQ(ArraySizeInBytes(Arr), sizeof(Foo) * 3);
+}
+
+TEST_F(AxArrayTest, NeedsGrowth)
+{
+    EXPECT_TRUE(ArrayNeedsGrowth(Arr, 18));
+}
+
+// [Element Access]
+
+TEST_F(AxArrayTest, Begin)
+{
+    EXPECT_EQ(ArrayBegin(Arr).a, 1);
+}
+
+TEST_F(AxArrayTest, End)
+{
+    Foo *end = Arr + ArraySize(Arr);
+    EXPECT_EQ(ArrayEnd(Arr), end);
+}
+
+TEST_F(AxArrayTest, Back)
+{
+    EXPECT_EQ(ArrayBack(Arr)->a, 3);
+}
+
+TEST_F(AxArrayTest, Pop)
+{
+    EXPECT_EQ(ArrayPop(Arr).a, 3);
+    EXPECT_EQ(ArrayPop(Arr).a, 2);
+    EXPECT_EQ(ArrayPop(Arr).a, 1);
+}
+
+TEST_F(AxArrayTest, NthAccess)
+{
+    EXPECT_EQ(Arr[0].a, 1);
+    EXPECT_EQ(Arr[1].a, 2);
+    EXPECT_EQ(Arr[2].a, 3);
+    EXPECT_STREQ(Arr[0].b, "Fly,");
+    EXPECT_STREQ(Arr[1].b, "you");
+    EXPECT_STREQ(Arr[2].b, "fools!");
+}
+
+TEST_F(AxArrayTest, Last)
+{
+    EXPECT_EQ(ArrayBack(Arr)->a, 3);
+    EXPECT_STREQ(ArrayBack(Arr)->b, "fools!");
+}
+
+TEST_F(AxArrayTest, Push)
+{
+    int* IntArr = NULL;
+
+    ArrayPush(IntArr, 1);
+    ArrayPush(IntArr, 2);
+    ArrayPush(IntArr, 3);
+
+    EXPECT_EQ(IntArr[0], 1);
+    EXPECT_EQ(IntArr[1], 2);
+    EXPECT_EQ(IntArr[2], 3);
+
+    ArrayFree(IntArr);
+}
+
+TEST_F(AxArrayTest, PushArray)
+{
+    Foo *Arr2 = NULL;
+
+    ArrayPush(Arr2, FooConstruct(4, "You"));
+    ArrayPush(Arr2, FooConstruct(5, "cannot"));
+    ArrayPush(Arr2, FooConstruct(6, "pass"));
+
+    ArrayPushArray(Arr, Arr2);
+
+    EXPECT_EQ(Arr[3].a, 4);
+    EXPECT_EQ(Arr[4].a, 5);
+    EXPECT_EQ(Arr[5].a, 6);
+    EXPECT_STREQ(Arr[3].b, "You");
+    EXPECT_STREQ(Arr[4].b, "cannot");
+    EXPECT_STREQ(Arr[5].b, "pass");
+
+    EXPECT_EQ(ArraySize(Arr), 6);
+
+    ArrayFree(Arr2);
+}
+
+TEST_F(AxArrayTest, Free)
+{
+    ArrayFree(Arr);
+    EXPECT_EQ(ArraySize(Arr), 0);
+    ArrayPush(Arr, FooConstruct(1, "Hello"));
+    EXPECT_EQ(ArraySize(Arr), 1);
+    EXPECT_EQ(Arr[0].a, 1);
+    EXPECT_STREQ(Arr[0].b, "Hello");
 }
