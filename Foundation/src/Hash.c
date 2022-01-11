4b3271f7ee49b95c7a268a63ecbac37d909232e8
Michael DeForge <mddeforge@gmail.com>
2022-01-10 21:48:49 -0500

    Splitting out FNV1a hashing algorithm from HashTable into its own header and source file

---
+++ Foundation/src/Hash.c
@@ -0,0 +1,30 @@
+#include "Hash.h"
+
+uint64_t HashStringFNV1a(const char *String, uint64_t HashVal)
+{
+    unsigned char *s = (unsigned char *)String;	// unsigned string
+
+    // FNV-1a hash each octet of the string
+    while (*s)
+    {
+        HashVal ^= (uint64_t)*s++; // xor the bottom with the current octet
+        HashVal *= FNV_64_PRIME;   // Multiply by the 64-bit FNV magic prime mod 2^64
+    }
+
+    return (HashVal);
+}
+
+uint64_t HashBufferFNV1a(void *Buffer, size_t Length, uint64_t HashVal)
+{
+    unsigned char *bp = (unsigned char *)Buffer; // Start of buffer
+    unsigned char *be = bp + Length;             // Beyond end of buffer
+
+    // FNV-1a hash each octet of the buffer
+    while (bp < be)
+    {
+        HashVal ^= (uint64_t)*bp++; // xor the bottom with the current octet */
+        HashVal *= FNV_64_PRIME; // Multiply by the 64 bit FNV magic prime mod 2^64
+    }
+
+    return (HashVal);
+}
