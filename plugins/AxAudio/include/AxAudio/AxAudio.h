598e2dbfefba490a32db29dd5e53ac3197857215
Michael DeForge <mddeforge@gmail.com>
2022-01-16 20:43:25 -0500

    Adding a very much WIP AxAudio plugin

---
+++ plugins/AxAudio/include/AxAudio/AxAudio.h
@@ -0,0 +1,36 @@
+#pragma once
+
+#include "Foundation/Types.h"
+
+// Typical sampling rates for game audio are 48k, sometimes 44.1k
+// Mixer thread
+// Clips ~10s or longer should probably be streamed
+// WASAPI/ASIO
+
+/* Features
+* Resampling / Clipping / Limiting
+* 3D Panning / Attenuation
+
+*/
+
+typedef enum CompressionType { NONE };
+
+typedef struct AxAudioData
+{
+    void *Data;
+    enum CompressionType Type;
+} AxAudioData;
+
+#define AXON_AUDIO_API_NAME "AxoAudioAPI"
+
+struct AxAudioAPI
+{
+    void (*Init)(void);
+    void (*Update)(void);
+    void (*Term)(void);
+
+    void (*LoadSound)(const char *SoundName, bool IsLooping, bool ShouldStream);
+    void (*UnloadSound)(const char *SoundName);
+    void (*Set3DListener)(const AxVec3 *Position, const AxVec3 *Look, const AxVec3 *Up);
+    int32_t (*PlaySound)(const char *SoundName);
+};
---
+++ plugins/AxAudio/src/Win32Audio.c
@@ -0,0 +1,51 @@
+#include "Foundation/APIRegistry.h"
+#include "Foundation/Math.h"
+#include "Foundation/HashTable.h"
+#include "Foundation/Platform.h"
+#include "AxAudio.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+// NOTE(mdeforge): Uncomment to get some leak detection
+//#define _CRTDBG_MAP_ALLOC
+//#include <stdlib.h>
+//#include <crtdbg.h>
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN
+#endif
+
+HashTable *SoundMap;
+HashTable *ChannelMap;
+
+static void Init(void)
+{
+    SoundMap = CreateTable(16);
+    ChannelMap = CreateTable(16);
+}
+
+static void Update(void)
+{
+
+}
+
+static void Term(void)
+{
+
+}
+
+struct AxAudioAPI *WindowAPI = &(struct AxAudioAPI) {
+    .Init = Init,
+    .Update = Update,
+    .Term = Term
+};
+
+AXON_DLL_EXPORT void LoadPlugin(struct AxAPIRegistry *APIRegistry, bool Load)
+{
+    if (APIRegistry)
+    {
+        //PlatformAPI = APIRegistry->Get(AXON_PLATFORM_API_NAME);
+
+        APIRegistry->Set(AXON_AUDIO_API_NAME, WindowAPI, sizeof(struct AxAudioAPI));
+    }
+}
