c738d07496c58ec8a9ba77ed13ebddfe3d1fdf95
Michael DeForge <mddeforge@gmail.com>
2022-02-11 01:24:30 -0500

    Replaced AxHeap with AxLinearAllocator in APIRegistry

--- foundation/src/APIRegistry.c
+++ foundation/src/APIRegistry.c
@@ -1,7 +1,7 @@
 #include "APIRegistry.h"
 #include "Plugin.h"
 #include "Platform.h"
-#include "AxHeap.h"
+#include "AxLinearAllocator.h"
 #include "ImageLoader.h"
 #include "AxHashTable.h"
 #include <string.h>
@@ -69,7 +69,7 @@ void AxonRegisterAllFoundationAPIs(struct AxAPIRegistry *APIRegistry)
     {
         APIRegistry->Set(AXON_PLUGIN_API_NAME, AxPluginAPI, sizeof(struct AxPluginAPI));
         APIRegistry->Set(AXON_PLATFORM_API_NAME, AxPlatformAPI, sizeof(struct AxPlatformAPI));
-        APIRegistry->Set(AXON_HEAP_API_NAME, AxHeapAPI, sizeof(struct AxHeapAPI));
+        APIRegistry->Set(AXON_LINEAR_ALLOCATOR_API_NAME, AxLinearAllocatorAPI, sizeof(struct AxLinearAllocatorAPI));
     }
 }
 
